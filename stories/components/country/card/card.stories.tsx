7faa2ea0822f3ffc65970ff41ae886a7844bf633
John Paulo Panganiban <olopangs@gmail.com>
2021-10-19 19:57:36 +0800

    refactor: fixed card.stories.tsx import errors

--- stories/components/country/card/card.stories.tsx
+++ stories/components/country/card/card.stories.tsx
@@ -2,20 +2,20 @@ import React from "react";
 import { Meta } from '@storybook/react'
 
 // Component
-import { Card } from "../../../../components/country/card/card";
+import { Card as CountryCard } from "../../../../components/country/card/card";
 
 // Sample Country
 import { sampleCountry } from "../../../../constants/sampleCountry";
 
 export default {
     title: 'Components/Country',
-    component: Card
+    component: CountryCard
 } as Meta
 
 export const Card = () => {
 
     return (
-        <Card
+        <CountryCard
             name={sampleCountry.name.common}
             capital={sampleCountry.capital}
             cca3={sampleCountry.cca3}
@@ -23,7 +23,7 @@ export const Card = () => {
             population={sampleCountry.population}
             region={sampleCountry.region}
         >
-        </Card>
+        </CountryCard>
     )
 
 }
