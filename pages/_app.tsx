5c18ea06e2bb77ea4fe4480eab1f6849c75a23e5
John Paulo Panganiban <olopangs@gmail.com>
2021-10-21 14:11:57 +0800

    refactor: deleted context, pages/api/allcountries.ts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
-
-// Contexts
-import { AllCountriesProvider } from '../contexts/countriesContext'
-
-// Theme
 import { theme } from '../theme/theme'
-
-// Components
 import { Header } from '../components/header/header'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <AllCountriesProvider>
       <ThemeProvider theme={theme}>
         <Header/>
         <Component {...pageProps} />
       </ThemeProvider>
-    </AllCountriesProvider>
   )
 }
 
