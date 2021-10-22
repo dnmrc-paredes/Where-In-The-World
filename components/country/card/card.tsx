d33626b86d32c75832e100ff1011cc5cadae7308
John Paulo Panganiban <olopangs@gmail.com>
2021-10-22 17:27:16 +0800

    refactor: styling tweaks on cardStyles.ts

--- components/country/card/card.tsx
+++ components/country/card/card.tsx
@@ -1,39 +1,45 @@
 import React, { FC } from "react";
-import Image from 'next/image'
-import Link from 'next/link'
-import { commaNumber } from '../../../utils/numComma'
-type Props = {
-    name: string
-    population: number
-    region: string
-    capital: string[]
-    img: string
-    cca3: string
-}
+import Image from "next/image";
+import Link from "next/link";
+import { commaNumber } from "../../../utils/numComma";
 import * as S from "./cardStyles";
-
+type Props = {
+    name: string;
+    population: number;
+    region: string;
+    capital: string[];
+    img: string;
+    cca3: string;
+  };
 export const CountryCard: FC<Props> = (props) => {
+  const { name, population, region, capital, img } = props;
 
-    const { name, population, region, capital, img } = props
-
-    return (
-        <Link passHref href={`/country/${name}`}>
-            <S.Card>
-                <S.ImageHolder>
-                    <Image placeholder="blur" blurDataURL={img} height={150} width={250} alt={name} src={img} />
-                </S.ImageHolder>
-                <S.CountryInfo>
-                    <S.CountryDetails>
-                        <S.Title> {name.length < 15 ? name : `${name.substring(0,15)}...`} </S.Title>
-                        <S.Detail> Population: {commaNumber(population)} </S.Detail>
-                        <S.Detail> Region: {region} </S.Detail>
-                        { capital ? 
-                        <S.Detail> Capital: {capital[0]} </S.Detail>
-                        : <S.Detail> Capital: N/A </S.Detail> }
-                    </S.CountryDetails>
-                </S.CountryInfo>
-            </S.Card>
-        </Link>
-    )
-
-}
+  return (
+    <Link passHref href={`/country/${name}`}>
+      <S.Card>
+          <Image
+            placeholder="blur"
+            blurDataURL={img}
+            height={150}
+            width={250}
+            alt={name}
+            src={img}
+          />
+        <S.CountryInfo>
+          <div>
+            <h1>
+              {name.length < 15 ? name : `${name.substring(0, 15)}...`}
+            </h1>
+            <p> Population: {commaNumber(population)} </p>
+            <p> Region: {region} </p>
+            {capital ? (
+              <p> Capital: {capital[0]} </p>
+            ) : (
+              <p> Capital: N/A </p>
+            )}
+          </div>
+        </S.CountryInfo>
+      </S.Card>
+    </Link>
+  );
+};
