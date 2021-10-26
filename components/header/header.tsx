8880817e375d0be4cb687d826aae14088da9c4b2
John Paulo Panganiban <olopangs@gmail.com>
2021-10-26 16:58:10 +0800

    refactor: styles and dark mode fix

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,37 +1,30 @@
 import React, { Dispatch, FC, SetStateAction } from "react";
-import { IoMoonOutline, IoMoon } from "react-icons/io5";
+import { IoMoon, IoMoonOutline } from "react-icons/io5";
 import { useRouter } from "next/router";
 import * as S from "./headerStyles";
-
 type Props = {
-  changeTheme: Dispatch<SetStateAction<"light" | "dark" | undefined>>;
-  value: "light" | "dark" | undefined;
+    changeTheme: Dispatch<SetStateAction<"light" | "dark" | undefined>>;
+    value: "light" | "dark" | undefined;
 };
-
 export const Header: FC<Props> = ({ changeTheme, value }) => {
   const router = useRouter();
-
   const toggleTheme = () => {
     localStorage.setItem("themeMode", value === "light" ? "dark" : "light");
     changeTheme((prev) => (prev === "light" ? "dark" : "light"));
-  };
-
+};
   return (
     <S.Navbar>
-      <S.NavTitle>
-        <S.Title onClick={() => router.push("/")}>Where in the world?</S.Title>
-      </S.NavTitle>
+      <a onClick={() => router.push("/")}>
+        <h2>Where in the world?</h2>
+      </a>
       <S.ToggleTheme>
-        {value === "light" ? (
-          <IoMoonOutline size={20} />
-        ) : (
-          <IoMoon color="white" size={20} />
-        )}
-        <S.ThemeText data-testid="toggleTheme" onClick={toggleTheme}>
-          {" "}
-          Dark Mode{" "}
-        </S.ThemeText>
-      </S.ToggleTheme>
+                {value === "light" ? (
+                    <IoMoonOutline size={20} />
+                ) : (
+                    <IoMoon color="white" size={20} />
+                )}
+                <S.ThemeText data-testid="toggleTheme" onClick={toggleTheme}> Dark Mode </S.ThemeText>
+            </S.ToggleTheme>
     </S.Navbar>
   );
 };
