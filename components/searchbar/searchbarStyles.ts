983b524bef61dd5bbb6c93f55601522f75785b94
John Paulo Panganiban <olopangs@gmail.com>
2021-10-26 17:21:21 +0800

    refactor: fixing color theme on searchBar.tsx

--- components/searchbar/searchbarStyles.ts
+++ components/searchbar/searchbarStyles.ts
@@ -1,41 +1,50 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const Base = styled.div`
     display: flex;
     justify-content: center;
-    align-items: center;
     height: 40px;
     width: 500px;
-    box-shadow: 1px 1px 10px 0px #E0DCDC;
     border-radius: 5px;
     overflow: hidden;
     @media screen and (max-width: 800px) {
         width: auto;
     }
-    > span {
-    flex: 1;
-    text-align: center;
-    background-color: white;
-    margin: 3px 0 0 0;
+`;
+
+export const Icon = styled.div(({ theme }) => ({
+    flex: 1,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: theme.elements,
+    '@media screen and (max-width: 800px)': {
+        flex: 2.5
     }
-`
+}));
 
 export const SearchBase = styled.div`
     flex: 10;
     display: flex;
+    background-color: green;
     @media screen and (max-width: 800px) {
         flex: 10;
     }
-`
+`;
 
-export const SearchInput = styled.input`
-    border: none;
-    outline: none;
-    flex: 1;
-    padding: 0 10px;
-    font-weight: 300;
-    &::placeholder {
-        color: gray;
+export const SearchInput = styled.input(({ theme }) => ({
+    border: 'none',
+    outline: 'none',
+    flex: 1,
+    padding: '0 10px',
+    fontWeight: 300,
+    backgroundColor: theme.elements,
+    color: theme.txtColor,
+    '&::placeholder': {
+        color: theme.txtColor
     }
+}))
 
-`
+
+
+
