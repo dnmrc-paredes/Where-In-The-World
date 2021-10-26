7dc87f31e0a383dbabd6b98600ed45119d23133b
John Paulo Panganiban <olopangs@gmail.com>
2021-10-26 15:16:32 +0800

    refactor: Styled components object syntax

--- components/country/card/cardStyles.ts
+++ components/country/card/cardStyles.ts
@@ -1,28 +1,45 @@
 import styled from "styled-components";
-export const Card = styled.div`
-    height: 330px;
-    width: 250px;
-    border-radius: 5px;
-    margin: 37.5px;
-    overflow: hidden;
-    box-shadow: 0px 3px 15px 2px #EDEDED;
-    background-color: ${props => props.theme.bg.lightMode};
-    cursor: pointer;
-`
+
+export const Card = styled.div(({theme}) => ({
+    height: "330px",
+    width: "250px",
+    borderRadius: "5px",
+    margin: "37.5px",
+    overflow: "hidden",
+    backgroundColor: theme.elements,
+    cursor: "pointer",
+}));
+
+export const Title = styled.h1(({theme}) => ({
+    fontSize: "20px",
+    marginBottom: "10px",
+    color: theme.txtColor,
+}));
+
+export const Detail = styled.p(({theme}) => ({
+    margin: "2px 0",
+    fontSize: "14px",
+    fontWeight: 300,
+    color: theme.txtColor,
+}));
+
+export const ImageHolder = styled.div`
+    flex: 1;
+    display: flex;
+    height: 150px;
+`;
+
 export const CountryInfo = styled.section`
+    flex: 2;
     display: flex;
     justify-content: center;
     height: 170px;
-    > div {
-        flex: 1;
-        margin: auto 25px;
-    > h1 {
-        font-size: 20px;
-        margin-bottom: 10px;
-        word-wrap: break-word;
-    }
-    > p {
-        margin: 2px 0;
-    }
-    }
-`
+`;
+
+export const CountryDetails = styled.div`
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    margin: auto 25px;
+`;
