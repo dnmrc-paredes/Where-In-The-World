d1828ef46e0d3665de9c198a7e6f1105e052df79
John Paulo Panganiban <olopangs@gmail.com>
2021-10-26 16:56:11 +0800

    refactor: styles and dark mode fix

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,30 +1,37 @@
 import React, { Dispatch, FC, SetStateAction } from "react";
-import { IoMoon, IoMoonOutline } from "react-icons/io5";
+import { IoMoonOutline, IoMoon } from "react-icons/io5";
 import { useRouter } from "next/router";
 import * as S from "./headerStyles";
+
 type Props = {
-    changeTheme: Dispatch<SetStateAction<"light" | "dark" | undefined>>;
-    value: "light" | "dark" | undefined;
+  changeTheme: Dispatch<SetStateAction<"light" | "dark" | undefined>>;
+  value: "light" | "dark" | undefined;
 };
+
 export const Header: FC<Props> = ({ changeTheme, value }) => {
   const router = useRouter();
+
   const toggleTheme = () => {
     localStorage.setItem("themeMode", value === "light" ? "dark" : "light");
     changeTheme((prev) => (prev === "light" ? "dark" : "light"));
-};
+  };
+
   return (
     <S.Navbar>
-      <a onClick={() => router.push("/")}>
-        <h2>Where in the world?</h2>
-      </a>
+      <S.NavTitle>
+        <S.Title onClick={() => router.push("/")}>Where in the world?</S.Title>
+      </S.NavTitle>
       <S.ToggleTheme>
-                {value === "light" ? (
-                    <IoMoonOutline size={20} />
-                ) : (
-                    <IoMoon color="white" size={20} />
-                )}
-                <S.ThemeText data-testid="toggleTheme" onClick={toggleTheme}> Dark Mode </S.ThemeText>
-            </S.ToggleTheme>
+        {value === "light" ? (
+          <IoMoonOutline size={20} />
+        ) : (
+          <IoMoon color="white" size={20} />
+        )}
+        <S.ThemeText data-testid="toggleTheme" onClick={toggleTheme}>
+          {" "}
+          Dark Mode{" "}
+        </S.ThemeText>
+      </S.ToggleTheme>
     </S.Navbar>
   );
 };
--- components/header/headerStyles.ts
+++ components/header/headerStyles.ts
@@ -1,39 +1,45 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
-export const Navbar = styled.nav`
-    text-align: center;
-    padding: 1rem;
-    height: 70px;
+export const Navbar = styled.nav(({ theme }) => ({
+    padding: '1rem',
+    height: '70px',
+    display: 'flex',
+    boxShadow: '0px 3px 2px 0px #ebebeb',
+    backgroundColor: theme.elements,
+
+    '@media screen and (max-width: 550px)': {
+        height: '100px'
+    }
+}))
+
+export const NavTitle = styled.div`
+    flex: 1;
     display: flex;
-    box-shadow: 0px 3px 2px 0px #EBEBEB;
-    @media screen and (max-width: 550px) {
-        height: 100px;
+    align-items: center;
+`;
+
+export const Title = styled.h2(({ theme }) => ({
+    color: theme.txtColor,
+    marginLeft: '3vw',
+    fontWeight: 800,
+    cursor: 'pointer',
+
+    '@media screen and (max-width: 550px)': {
+        fontSize: '15px'
     }
-    > h2 {
-        margin-left: 3vw;
-        font-weight: 800;
-        cursor: pointer;
-    }
-`
-export const Title = styled.h2`
-    color: black;
-    margin-left: 3vw;
-    font-weight: 800;
-    cursor: pointer;
-    @media screen and (max-width: 550px) {
-        font-size: 15px;
-    }
-`
+}))
+
 export const ToggleTheme = styled.div`
     flex: 1;
     display: flex;
     justify-content: flex-end;
     align-items: center;
     cursor: pointer;
-`
-export const ThemeText = styled.p`
-    font-weight: 600;
-    margin-right: 3vw;
-    margin-left: 5px;
-    font-weight: 800;
-`
+`;
+
+export const ThemeText = styled.p(({ theme }) => ({
+    fontWeight: 800,
+    marginRight: '3vw',
+    marginLeft: '5px',
+    color: theme.txtColor
+}));
